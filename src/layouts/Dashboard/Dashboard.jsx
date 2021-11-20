b3eb178d219fbf46a021729f2f733b2dc724bdbf
simba0724 <bremaster0806@gmail.com>
2021-11-19 16:45:07 -0800

    alsdkfsdajkl

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -58,8 +58,8 @@ function TablePaginationActions(props) {
   );
 }
 
-export default function Dashboard({provider}) {
-let account = "0x54cf3aeB19f974DA2831147022e9A8BBBff17527";
+export default function Dashboard({account, provider}) {
+  
   const [rows, setRows] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [rewardtokenadd, setRewardtokenadd] = React.useState('');
